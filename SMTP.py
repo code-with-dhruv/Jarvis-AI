8638dd7732d3449188a06dd9f221cb34f47da09c
dhruv <87694090+code-with-dhruv@users.noreply.github.com>
2022-05-12 00:14:27 +0530

    Create SMTP.py

---
+++ SMTP.py
@@ -0,0 +1,7 @@
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('youremail@gmail.com', 'your-password')
+    server.sendmail('youremail@gmail.com', to, content)
+    server.close()
