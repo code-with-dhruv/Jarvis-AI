1a3f8d8e5692b92b8cb35febd8f0e1133d47a55a
dhruv <87694090+code-with-dhruv@users.noreply.github.com>
2022-05-12 00:13:21 +0530

    Create speak function.py

---
+++ speak function.py
@@ -0,0 +1,17 @@
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+import datetime
+import wikipedia #pip install wikipedia
+import webbrowser
+import os
+import smtplib
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
